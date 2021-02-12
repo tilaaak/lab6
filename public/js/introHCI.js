0d85d51bcc83604684a41e2e4f10086a199c8c7e
Tilak Patel <tpatel@ucsd.edu>
2021-02-11 17:30:41 -0800

    ajax and facebook lab

--- public/js/introHCI.js
+++ public/js/introHCI.js
@@ -20,11 +20,35 @@ function initializePage() {
 function addProjectDetails(e) {
 	// Prevent following the link
 	e.preventDefault();
+	
 
 	// Get the div ID, e.g., "project3"
 	var projectID = $(this).closest('.project').attr('id');
 	// get rid of 'project' from the front of the id 'project3'
 	var idNumber = projectID.substr('project'.length);
+	
+	$.get(" http://localhost:3000/project/" + idNumber, callBackFn);
 
+	
+	
 	console.log("User clicked on project " + idNumber);
+
+	
+}
+
+/*'<a href="#" class="thumbnail">' +
+'<img src="' + projects['image'] + '" class="detailsImage">' +
+'<p>' + projects['title'] + '</p>' +
+'<p><small>' + projects['date'] +
+'</small>' + projects['summary'] + '</p></a>'
+*/
+function callBackFn(projectData){
+	console.log(projectData);
+	var idNum = projectData.id;
+	console.log(idNum);
+	$('#project'+ idNum +' .details').html(
+	'<img src="' + projectData['image'] + '" class="detailsImage">' +
+	'<p>' + projectData['title'] + '</p>' +
+	'<p><small>' + projectData['date'] +
+	'</small>' + projectData['summary'] + '</p>');
 }
